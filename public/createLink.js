7241bef89a2138e5e5656ca73b49a207f692cc14
Manoj <manoj@manojs-mbp.home>
2018-02-25 20:19:47 +0000

    cleanup

--- public/createLink.js
+++ public/createLink.js
@@ -1,59 +1,30 @@
 // Make connection
-var socket = io.connect('http://localhost:4001');
+var socket = io.connect('http://localhost:4000');
 
-var scrum = document.getElementById('scrum'),
-	story = document.getElementById('story')
-	  btn = document.getElementById('submit'),
-      output = document.getElementById('output');
+var scrum = document.getElementById('scrum'), story = document
+		.getElementById('story')
+btn = document.getElementById('submit'), output = document
+		.getElementById('output');
 
-
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function() {
 	console.log(scrum.value);
 	console.log(story.value);
-	  socket.emit('createLink', {
-	      scrum: scrum.value,
-	      story: story.value
-	  });
+	socket.emit('createLink', {
+		scrum : scrum.value,
+		story : story.value
 	});
-
-
-
-
-
-	// Listen for events
-	socket.on('link', function(data){
-		console.log('Inside inner HTML');
-	    output.innerHTML += '<p><strong>http://localhost:4001/' + data.scrum + '/' + data.story + '</p>';
-	   /* socket.emit('estimate', {
-		      scrum: scrum.value,
-		      story: story.value
-		  });*/
-	    window.location.href = 'http://localhost:4001/estimate.html?scrum='+data.scrum+'&story='+data.story; 
-	    
-	   
-	    
-	});
-	
-
-/*
-// Query DOM
-var message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
-      btn = document.getElementById('send'),
-      output = document.getElementById('output');
-
-// Emit events
-btn.addEventListener('click', function(){
-  socket.emit('chat', {
-      message: message.value,
-      handle: handle.value
-  });
-  message.value = "";
 });
 
 // Listen for events
-socket.on('chat', function(data){
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+socket.on('link', function(data) {
+	console.log('Inside inner HTML');
+	output.innerHTML += '<p><strong>http://localhost:4000/' + data.scrum + '/'
+			+ data.story + '</p>';
+	/* socket.emit('estimate', {
+	      scrum: scrum.value,
+	      story: story.value
+	  });*/
+	window.location.href = 'http://localhost:4000/estimate.html?scrum='
+			+ data.scrum + '&story=' + data.story;
+
 });
-	
-	*/
